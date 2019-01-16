b7d1123842554835212d5112176ecdb4e7fed643
atul-sachan <34751474+atul-sachan@users.noreply.github.com>
2019-01-17 04:35:13 +0530

    second commit

--- KendoDemo-UI/src/app/kendo-grid-example/kendo-grid-example.component.ts
+++ KendoDemo-UI/src/app/kendo-grid-example/kendo-grid-example.component.ts
@@ -1,15 +1,115 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
+import { GridComponent, PageChangeEvent, SelectionEvent } from '@progress/kendo-angular-grid';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { KendoGridServiceService } from './kendo-grid-service.service';
+import { tap, switchMap } from 'rxjs/operators';
+import { SortDescriptor } from '@progress/kendo-data-query';
 
 @Component({
   selector: 'app-kendo-grid-example',
   templateUrl: './kendo-grid-example.component.html',
-  styleUrls: ['./kendo-grid-example.component.css']
+  styleUrls: ['./kendo-grid-example.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class KendoGridExampleComponent implements OnInit {
+  public loading: boolean;
+  @ViewChild('grid') private grid: GridComponent;
+  public mySelection: string[] = [];
 
-  constructor() { }
+  public state: any = {
+    skip: 0,
+    take: 100,
+    sort: [{
+      field: 'Id',
+      dir: 'asc'
+    }]
+  };
+
+  public query: any;
+  private stateChange = new BehaviorSubject<any>(this.state);
+
+  constructor(private service: KendoGridServiceService) { }
 
   ngOnInit() {
+    this.query = this.stateChange.pipe(
+      tap(state => {
+        this.state = state;
+        this.loading = true;
+      }),
+      switchMap(state => this.service.fetch(state)),
+      tap(() => {
+        // this.rowPersistance();
+        this.loading = false;
+      })
+    );
+  }
+
+  public pageChange(state: PageChangeEvent): void {
+    this.state.take = state.take;
+    this.state.skip = state.skip;
+    this.stateChange.next(this.state);
+  }
+
+  public sortChange(sort: SortDescriptor[]): void {
+    const sorts = sort.filter(e => {
+      if (e.dir === undefined) {
+        e.dir = 'asc';
+      }
+      return e;
+    });
+    this.state.sort = sorts;
+    this.stateChange.next(this.state);
+  }
+
+  public onSelectedKeysChange(e) { }
+
+  public onSelectedChange(state: SelectionEvent) {
+    localStorage.setItem('id', state.selectedRows[0].dataItem.Id);
+  }
+  public met() {
+    this.mySelection = [];
+    this.mySelection.push('03baa64c-359f-471c-84a8-aee06d32a4f0');
+    setTimeout(() => {
+      document.querySelector('.k-state-selected').scrollIntoView();
+    }, 3000);
+
+  }
+
+  public manualselect() {
+    this.mySelection = [];
+    this.mySelection.push('3e46cd1f-8750-4dd7-8d75-758e49829a09');
+    this.state.skip = 300;
+    this.stateChange.next(this.state);
+    setTimeout(() => {
+      document.querySelector('.k-state-selected').scrollIntoView();
+    }, 3000);
+  }
+
+  select() {
+    this.rowPersistance();
+    // 5e02fac8-dbec-4f4a-9a33-ba48f8d2c9f1
+  }
+
+  public rowPersistance(): void {
+    if (localStorage.getItem('index')) {
+
+      const currentPage = (this.state.skip / 100);
+      // tslint:disable-next-line:radix
+      const persistancePage = Math.floor(parseInt(localStorage.getItem('index')) / 100);
+      setTimeout(() => {
+        this.mySelection = [];
+        this.mySelection.push(localStorage.getItem('id').toString());
+        if (currentPage === persistancePage) {
+
+          console.log(document.querySelector('.k-state-selected'));
+          document.querySelector('.k-state-selected').scrollIntoView();
+        } else {
+          // tslint:disable-next-line:radix
+          this.state.skip = parseInt(localStorage.getItem('index')) - 1;
+          this.stateChange.next(this.state);
+        }
+      }, 4000);
+    }
   }
 
 }
--- KendoDemo-UI/src/app/kendo-grid-example/kendo-grid-service.service.ts
+++ KendoDemo-UI/src/app/kendo-grid-example/kendo-grid-service.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GridDataResult } from '@progress/kendo-angular-grid';
+import { toODataString } from '@progress/kendo-data-query';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators/map';
+
+export abstract class DataService {
+  private BASE_URL = 'http://localhost:5000/odata/';
+
+  constructor(
+    private http: HttpClient,
+    protected tableName: string
+  ) {
+  }
+
+  public fetch(state: any): Observable<GridDataResult> {
+    const queryStr = `${toODataString(state)}&$count=true`;
+
+    return this.http
+      // tslint:disable-next-line:max-line-length
+      .get(`${this.BASE_URL}${this.tableName}?${queryStr}`, { 'headers': new HttpHeaders({ 'id': localStorage.getItem('id') }), observe: 'response' })
+      .pipe(
+        map(response => {
+          const gridData = response.body['value'];
+          // gridData.map((val, index) => val.RowId = index);
+          localStorage.setItem('index', response.headers.get('X-Total-Count'));
+          return (<GridDataResult>{
+            data: gridData,
+            total: parseInt(response.body['@odata.count'], 10)
+          });
+        })
+      );
+  }
+}
+
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class KendoGridServiceService {
+export class KendoGridServiceService extends DataService {
 
-  constructor() { }
+  constructor(http: HttpClient) { super(http, 'Kendo'); }
 }
